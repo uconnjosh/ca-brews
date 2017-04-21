48e16061773dedada0014645388850e2412203da
Josh Paul <jpaul@focusvision.com>
2017-04-20 23:31:58 -0700

    Add brew attributes

--- app/models/brew.js
+++ app/models/brew.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  brewery: DS.attr('string'),
+  city: DS.attr('string'),
+  name: DS.attr('string'),
+  abv: DS.attr('string'),
+  ibu: DS.attr('string'),
+  srm: DS.attr('string'),
+  tags: DS.attr('string')
 });
