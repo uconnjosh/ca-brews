8ea0e1f185629626cfcb77f351d8de792e41bd72
Josh Paul <jpaul@focusvision.com>
2017-04-24 12:25:27 -0700

    Cleanup jshint errors. Remove unneeded application serializer declaration.

--- app/controllers/beers.js
+++ app/controllers/beers.js
@@ -3,13 +3,13 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   filteredBeer: Ember.computed('model', 'nameFilter', function() {
     if (this.get('nameFilter')) {
-      let nameFilter = this.get('nameFilter')
+      let nameFilter = this.get('nameFilter');
       return this.get('model').filter(function(item) {
-        let nameValue = item.get('Name')
-       return nameValue.indexOf(nameFilter) >= 0
-      })
+        let nameValue = item.get('Name');
+       return nameValue.indexOf(nameFilter) >= 0;
+      });
     } else {
-      return this.get('model')
+      return this.get('model');
     }
   }),
   nameFilter: null
--- app/serializers/beer.js
+++ app/serializers/beer.js
@@ -1,12 +1,11 @@
-import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
   normalizeArrayResponse: function(store, primaryModelClass, payload, id, requestType) {
     const payloadWithTags = payload.map(function(object) {
-      return tagObjects(object)
-    })
-    const payloadWithRoot = { beers: payloadWithTags }
+      return tagObjects(object);
+    });
+    const payloadWithRoot = { beers: payloadWithTags };
     return this._super(store, primaryModelClass, payloadWithRoot, id, requestType);
   },
   primaryKey: 'Name',
@@ -16,11 +15,11 @@ export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
 });
 
 var tagObjects = function(beerObject) {
-  let tags = beerObject["Tags"].split("|")
+  let tags = beerObject["Tags"].split("|");
   let mapped_tags = tags.map(function(tag) {
-    return { "id": tag }
+    return { "id": tag };
   });
 
-  beerObject["tags"] = mapped_tags
-  return beerObject
-}
+  beerObject["tags"] = mapped_tags;
+  return beerObject;
+};
